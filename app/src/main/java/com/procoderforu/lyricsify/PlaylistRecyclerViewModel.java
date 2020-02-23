22b3e1833776097a7c53aa96a5112c34fa459e90
asharam meena <asharam1234meena@gmail.com>
2020-02-23 18:10:21 +0530

    designed layout of home screen

---
+++ app/src/main/java/com/procoderforu/lyricsify/PlaylistRecyclerViewModel.java
@@ -0,0 +1,14 @@
+package com.procoderforu.lyricsify;
+
+public class PlaylistRecyclerViewModel {
+
+    private String playListName;
+
+    public PlaylistRecyclerViewModel(String playListName) {
+        this.playListName = playListName;
+    }
+
+    public String getPlayListName() {
+        return playListName;
+    }
+}
